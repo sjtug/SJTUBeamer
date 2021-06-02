0a2ffdc31c29e3f73b53f40f014fb8eee0075cfd
Alex Chi <iskyzh@gmail.com>
2021-06-02 10:53:49 +0800

    ci: build all variants

--- .github/ci/build_all_variants.sh
+++ .github/ci/build_all_variants.sh
@@ -2,24 +2,14 @@
 
 set -e
 
-sed -i.bak -e "s|usetheme\[.*\]|usetheme\[bannertitle,blue\]|g" main.tex
-latexmk -C
-latexmk -xelatex
-cp main.pdf build/main-bannertitle-blue.pdf
+sed -e "s|usetheme\[.*\]|usetheme\[bannertitle,blue\]|g" main.tex > build-bannertitle-blue.tex
+sed -e "s|usetheme\[.*\]|usetheme\[bannertitle,red\]|g" main.tex  > build-bannertitle-red.tex
+sed -e "s|usetheme\[.*\]|usetheme\[blue\]|g" main.tex             > build-blue.tex
+sed -e "s|usetheme\[.*\]|usetheme\[red\]|g" main.tex              > build-red.tex
 
-sed -i.bak -e "s|usetheme\[.*\]|usetheme\[bannertitle,red\]|g" main.tex
 latexmk -C
-latexmk -xelatex
-cp main.pdf build/main-bannertitle-red.pdf
+latexmk $@ build-*.tex
 
-sed -i.bak -e "s|usetheme\[.*\]|usetheme\[blue\]|g" main.tex
-latexmk -C
-latexmk -xelatex
-cp main.pdf build/main-blue.pdf
-
-sed -i.bak -e "s|usetheme\[.*\]|usetheme\[red\]|g" main.tex
-latexmk -C
-latexmk -xelatex
-cp main.pdf build/main-red.pdf
-
-sed -i.bak -e "s|usetheme\[.*\]|usetheme\[bannertitle,blue\]|g" main.tex
+mkdir -p build
+mv build-*.pdf build/
+rm build-*.tex
