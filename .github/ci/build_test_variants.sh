b8411ec6b9d01b2b9631a2dbffb86e1630a11b8c
Alex Chi <iskyzh@gmail.com>
2021-06-02 13:23:51 +0800

    fix ci

--- .github/ci/build_test_variants.sh
+++ .github/ci/build_test_variants.sh
@@ -8,4 +8,4 @@ mkdir -p build
 sed -e "s|usetheme\[.*\]|usetheme\[bannertitle,blue\]|g" main.tex > build/build-bannertitle-blue.tex
 sed -e "s|usetheme\[.*\]|usetheme\[red\]|g" main.tex              > build/build-red.tex
 
-latexmk $@ -outdir=build build-*.tex
+latexmk $@ -outdir=build build/build-*.tex
