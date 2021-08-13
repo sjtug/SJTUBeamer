e5663bfd3e8b58f1f6c9f7e22ffe90ed193bc53d
LogCreative <longlaoda123456@qq.com>
2021-08-13 13:43:37 +0800

    fix: standarize macrocode decl.

--- source/docstrip.py
+++
@@ -1,7 +0,0 @@
-with open("beamerthemeSJTUBeamermin.dtx","r",encoding="UTF-8") as f:
-    rows = f.readlines()
-    wf = open("beamerthemeSJTUBeamermin.sty","w",encoding="UTF-8")
-    for row in rows:
-        if not row[0]=="%":
-            wf.write(row)
-    wf.close()
