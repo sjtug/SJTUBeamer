f27f12cfff5bfdd263497f64295055dd6139cd34
LogCreative <lizilong123456@live.cn>
2021-08-13 21:58:48 +0800

    fix: ins

---
+++ build.lua
@@ -0,0 +1,8 @@
+#!/usr/bin/env texlua
+
+module           = "sjtubeamermin"
+
+sourcefiledir    = "source"
+docfiledir       = "doc"
+typesetfiles     = {"*.tex"}
+packtdszip       = true -- recommended for "tree" layouts
