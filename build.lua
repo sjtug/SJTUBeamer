740d8372f530caefcb3ba91943eebfd608005635
LogCreative <longlaoda123456@qq.com>
2021-08-13 23:35:51 +0800

    l3build: init test

--- build.lua
+++ build.lua
@@ -1,8 +1,19 @@
 #!/usr/bin/env texlua
+-- Windows MikTeX still have l3build bug!
+-- Use ubuntu to compile.
+-- https://github.com/latex3/l3build/issues/185
 
 module           = "sjtubeamermin"
 
 sourcefiledir    = "source"
 docfiledir       = "doc"
-typesetfiles     = {"*.tex"}
+sourcefiles      = {"*.ins","*.dtx","sjtucnlogo.pdf","sjtuenlogo.pdf"}
+installfiles      = {"*.sty","sjtucnlogo.pdf","sjtuenlogo.pdf"}
+
+typesetfiles     = {"sjtubeamermintheme.tex","sjtubeamermindevguide.tex"}
 packtdszip       = true -- recommended for "tree" layouts
+
+testfiledir      = "test"
+checkengines     = {"xetex"}
+stdengine        = "xetex"
+checkformat      = "latex"
