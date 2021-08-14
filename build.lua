a2b46a9803e9a8c076893975a2f9d7abba19a7d6
LogCreative <longlaoda123456@qq.com>
2021-08-14 12:37:58 +0800

    fix: ins license + feat: demo before doc

--- build.lua
+++ build.lua
@@ -1,4 +1,5 @@
 #!/usr/bin/env texlua
+
 -- Windows MikTeX still have l3build bug!
 -- Use ubuntu to compile.
 -- https://github.com/latex3/l3build/issues/185
@@ -6,12 +7,13 @@
 module           = "sjtubeamermin"
 
 sourcefiledir    = "source"
-docfiledir       = "doc"
 sourcefiles      = {"*.ins","*.dtx","sjtucnlogo.pdf","sjtuenlogo.pdf"}
 installfiles     = {"*.sty","sjtucnlogo.pdf","sjtuenlogo.pdf"}
 
+docfiledir       = "doc"
 typesetexe       = "xelatex"
 typesetfiles     = {"sjtubeamermintheme.tex","sjtubeamermindevguide.tex"}
+typesetdemofiles = {"red.tex","blue.tex"}
 
 testfiledir      = "test"
 checkengines     = {"xetex"}
