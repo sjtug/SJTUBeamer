6a728bd31a2e8de7419fbd779686036aadd65cb0
LogCreative <longlaoda123456@qq.com>
2021-08-16 13:01:53 +0800

    fix: class warning

--- src/build.lua
+++ src/build.lua
@@ -15,7 +15,7 @@ docfiledir       = "doc"
 typesetexe       = "xelatex"
 typesetfiles     = {"sjtubeamermintheme.tex","sjtubeamermindevguide.tex"}
 typesetdemofiles = {"red.tex","blue.tex","my.tex","main.tex"}
-typesetsuppfiles = {"head.png","plant.jpg","test.csv","testgraph.tex","ref.bib","cnlogored.pdf","enlogored.pdf"}
+typesetsuppfiles = {"head.png","plant.jpg","test.csv","testgraph.tex","ref.bib","cnlogored.pdf","enlogored.pdf","contents/"}
 
 packtdszip       = true -- recommended for "tree" layouts
 
