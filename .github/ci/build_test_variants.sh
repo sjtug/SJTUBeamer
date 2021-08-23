cd5ed1e512b60100b2d29e80d47b99df45ce6a2d
LogCreative <lizilong123456@live.cn>
2021-08-24 06:23:17 +0800

    ci: testfile changed

--- .github/ci/build_test_variants.sh
+++ .github/ci/build_test_variants.sh
@@ -5,8 +5,8 @@ set -e
 rm -rf build
 mkdir -p build
 
-sed -e "s|usetheme\[.*\]|usetheme\[maxplus,light,blue\]|g" main.tex > build/build-blue.tex
-sed -e "s|usetheme\[.*\]|usetheme\[max,dark,red\]|g" main.tex              > build/build-red.tex
+sed -e "s|usetheme\[.*\]|usetheme\[maxplus,blue\]|g" main.tex > build/build-blue.tex
+sed -e "s|usetheme\[.*\]|usetheme\[max,light,red\]|g" main.tex              > build/build-red.tex
 sed -e "s|usetheme\[.*\]|usetheme\[min,dark,red\]|g" main.tex              > build/build-min.tex
 
 latexmk $@ -outdir=build build/build-*.tex
