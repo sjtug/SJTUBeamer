feb46b964b5e7953e21edfa6be6d63935c37582a
LogCreative <longlaoda123456@qq.com>
2021-08-24 12:27:40 +0800

    ci: build_package

--- .github/ci/build_package.sh
+++ .github/ci/build_package.sh
@@ -5,4 +5,12 @@ cd src
 l3build ctan
 cp -r -f build/distrib/tds/tex/latex/sjtubeamer/* ..
 
+# turn resource files into symbolic link.
+# TODO: a more elegant solution in the future.
 cd ..
+resources=(cnlogo.pdf dlogo.pdf enlogo.pdf sjtubadge.pdf sjtuphoto.jpg vlogo.pdf)
+for resource in ${resources[@]};
+do
+    rm -f $resource
+    ln -s src/source/$resource $resource
+done
