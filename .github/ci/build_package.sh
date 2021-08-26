d34f9b4d2f00fd9c9d878648abb267bce22e4207
Alex Chi <iskyzh@gmail.com>
2021-08-26 20:37:27 +0800

    Revert "fix bug"
    This reverts commit efb65e98ae85e948c118bccd9cc30c1ee9fb0f98.

--- .github/ci/build_package.sh
+++ .github/ci/build_package.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-set -e
-
 apk add zip
-cd src && l3build ctan
+cd src
+l3build ctan
+cp -r -f build/distrib/tds/tex/latex/sjtubeamer/* ..
+
+# turn resource files into symbolic link.
+# TODO: a more elegant solution in the future.
+cd ..
+resources=(cnlogo.pdf dlogo.pdf enlogo.pdf sjtubadge.pdf sjtuphoto.jpg vlogo.pdf sjtubg.png)
+for resource in ${resources[@]};
+do
+    rm -f $resource
+    ln -s src/source/$resource $resource
+done
