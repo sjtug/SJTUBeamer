eb454bb002b485670922436d0b5edc1187ed344f
LogCreative <lizilong123456@live.cn>
2021-09-01 13:30:17 +0800

    fix: gen_cover

--- src/support/tutourial/gen_outer.sh
+++ src/support/tutourial/gen_outer.sh
@@ -3,5 +3,5 @@
 outers=(default infolines smoothbars sidebar split shadow tree smoothtree)
 for outer in ${outers[@]};
 do
-    sed -e "s|<options>|$outer|g" step2miniframes.tex > step2$outer.tex
+    sed -e "s|miniframes|$outer|g" step2miniframes.tex > step2$outer.tex
 done
