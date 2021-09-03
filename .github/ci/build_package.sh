0b1193aa7c2029984da21d898b2d6d9a0948c0d4
LogCreative <lizilong123456@live.cn>
2021-09-04 04:27:56 +0800

    fix: ci

--- .github/ci/build_package.sh
+++ .github/ci/build_package.sh
@@ -5,7 +5,7 @@ l3build ctan
 # This will trigger checkinit_hook() to move the files to the root directory.
 
 # but doesn't move vi/ folder.
-cp -r -f src/source/vi vi/
+cp -r -f source/vi ../vi/
 
 if [ ! -f src/sjtubeamer-ctan.zip ]; then
     echo "BUILD FAILED."
