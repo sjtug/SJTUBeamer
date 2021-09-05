11623ff1086722848fcef2a57984f6883d08ba3d
LogCreative <lizilong123456@live.cn>
2021-09-05 17:34:43 +0800

    ci: test install

--- .github/ci/build_package.sh
+++ .github/ci/build_package.sh
@@ -11,3 +11,9 @@ if [ ! -f src/sjtubeamer-ctan.zip ]; then
     echo "BUILD FAILED."
     exit 1
 fi
+
+# test installation
+cd src
+l3build install
+cd src/doc
+latexmk min -halt-on-error -time -xelatex -outdir=build -shell-escape
