aa2ab3112ea2abcceff1d10266771be14ae50a2c
LogCreative <lizilong123456@live.cn>
2022-01-31 14:25:18 +0800

    ci: add contrib gen to release

--- .github/ci/gen_cover.sh
+++ .github/ci/gen_cover.sh
@@ -10,8 +10,24 @@ sed -e "s|<options>|red,dark|g" src/doc/cover.template.tex > build/cover-red-dar
 sed -e "s|<options>|blue,light|g" src/doc/cover.template.tex > build/cover-blue-light.tex
 sed -e "s|<options>|blue,dark|g" src/doc/cover.template.tex > build/cover-blue-dark.tex
 
+# compile covers
 latexmk $@ -outdir=build build/cover-*.tex
 
-# merge covers
+# copy cover main file with removed last line
 cp -f src/doc/cover.tex build/cover.tex
+sed -e "s|\\\end{document}||g" src/doc/cover.tex > build/cover.tex
+
+# contrib
+for theme in contrib/*;
+do
+    theme=${theme/contrib\//}
+    echo Compiling $theme ...
+    cp contrib/$theme/$theme.tex build/$theme.tex
+    latexmk $@ -outdir=build build/$theme.tex
+    echo \\includepdf{$theme.pdf}>>build/cover.tex
+done
+
+echo \\end{document}>>build/cover.tex
+
+# merge covers
 latexmk $@ -outdir=build build/cover.tex
