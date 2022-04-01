7d18cfde015f326f853f7568058918122fc2cc16
LogCreative <lizilong123456@live.cn>
2022-04-01 20:48:35 +0800

    feat: sectioning pages for maxplus & max (#91)

--- .github/ci/copy_resources.sh
+++ .github/ci/copy_resources.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 set -e
-resources=(cnlogo.pdf dlogo.pdf enlogo.pdf sjtubadge.pdf sjtuphoto.jpg vlogo.pdf sjtubg.png)
-for resource in ${resources[@]};
-do
-    rm -f $resource
-    cp src/source/vi/$resource vi/$resource
-done
+
+rm -rf vi/
+mkdir vi
+cp -r src/source/vi/* vi/
