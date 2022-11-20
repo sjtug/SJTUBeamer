ee9b87de831233bfacfc48649408d6d51d710d62
LogCreative <logcreative@outlook.com>
2022-11-20 11:42:49 +0800

    chore: move template files to support

--- .github/ci/gen_cover.sh
+++ .github/ci/gen_cover.sh
@@ -5,17 +5,17 @@ set -e
 mkdir -p build
 
 # covers
-sed -e "s|<options>|red,light|g" src/doc/cover.template.tex > build/cover-red-light.tex
-sed -e "s|<options>|red,dark|g" src/doc/cover.template.tex > build/cover-red-dark.tex
-sed -e "s|<options>|blue,light|g" src/doc/cover.template.tex > build/cover-blue-light.tex
-sed -e "s|<options>|blue,dark|g" src/doc/cover.template.tex > build/cover-blue-dark.tex
+sed -e "s|<options>|red,light|g" src/support/cover.template.tex > build/cover-red-light.tex
+sed -e "s|<options>|red,dark|g" src/support/cover.template.tex > build/cover-red-dark.tex
+sed -e "s|<options>|blue,light|g" src/support/cover.template.tex > build/cover-blue-light.tex
+sed -e "s|<options>|blue,dark|g" src/support/cover.template.tex > build/cover-blue-dark.tex
 
 # compile covers
 latexmk $@ -outdir=build build/cover-*.tex
 
 # copy cover main file with removed last line
-cp -f src/doc/cover.tex build/cover.tex
-sed -e "s|\\\end{document}||g" src/doc/cover.tex > build/cover.tex
+cp -f src/support/cover.tex build/cover.tex
+sed -e "s|\\\end{document}||g" src/support/cover.tex > build/cover.tex
 
 # contrib
 for theme in contrib/*;
