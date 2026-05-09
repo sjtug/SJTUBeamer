1411635611dd3883a744986c304dfd07a6a87c3f
LogCreative <logcreative@outlook.com>
2026-05-09 10:14:22 +0800

    ci: rename the sjtulib-talk release assets

--- .github/ci/build_all_variants.sh
+++ .github/ci/build_all_variants.sh
@@ -4,9 +4,9 @@ set -e
 
 mkdir -p build
 
-sed -e "s|usetheme\[.*\]|usetheme\[maxplus,light,blue\]|g" main.tex > build/build-maxplus-blue.tex
-sed -e "s|usetheme\[.*\]|usetheme\[maxplus,dark,red\]|g" main.tex  > build/build-maxplus-red.tex
-sed -e "s|usetheme\[.*\]|usetheme\[max,red\]|g" main.tex             > build/build-max-red.tex
-sed -e "s|usetheme\[.*\]|usetheme\[min,red\]|g" main.tex              > build/build-min-red.tex
+sed -e "s|usetheme\[.*\]|usetheme\[maxplus,light,blue\]|g" main.tex > build/sjtulib-talk-maxplus-blue.tex
+sed -e "s|usetheme\[.*\]|usetheme\[maxplus,dark,red\]|g" main.tex  > build/sjtulib-talk-maxplus-red.tex
+sed -e "s|usetheme\[.*\]|usetheme\[max,red\]|g" main.tex             > build/sjtulib-talk-max-red.tex
+sed -e "s|usetheme\[.*\]|usetheme\[min,red\]|g" main.tex              > build/sjtulib-talk-min-red.tex
 
-latexmk $@ -outdir=build build/build-*.tex
+latexmk $@ -outdir=build build/sjtulib-talk-*.tex
